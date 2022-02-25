66a8823b8da0e081d8af03f93c80543138ec9e46
Victor Rafaeli <victor.srafaeli@gmail.com>
2022-02-25 15:26:43 -0300

    Reestruturando o projeto

---
+++ scripts/imagens.js
@@ -0,0 +1,14 @@
+let imagemDaEstrada;
+let imagemDoAtor;
+let imagemCarro;
+let imagemCarro2;
+let imagemCarro3;
+
+function preload() {
+    imagemDaEstrada = loadImage("./resources/img/estrada.png");
+    imagemDoAtor = loadImage("./resources/img/ator-1.png");
+    imagemCarro = loadImage("./resources/img/carro-1.png");
+    imagemCarro2 = loadImage("./resources/img/carro-2.png");
+    imagemCarro3 = loadImage("./resources/img/carro-3.png");
+    imagemCarros = [imagemCarro, imagemCarro2, imagemCarro3];
+}
