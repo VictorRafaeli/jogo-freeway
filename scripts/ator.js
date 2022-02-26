2fdeb5b700fbec4efb490d6e8dc8f4f02fe26583
Victor Rafaeli <victor.srafaeli@gmail.com>
2022-02-26 13:39:39 -0300

    Ajusta posição inicial e colisão do ator

--- scripts/ator.js
+++ scripts/ator.js
@@ -12,13 +12,15 @@ function movimentaAtor() {
         yAtor -= 3;
     }
     if (keyIsDown(DOWN_ARROW)) {
-        yAtor += 3;
+        if (podeSeMover()) {
+            yAtor += 3;
+        }
     }
 }
 
 function verificaColisao() {
     for (let i = 0; i < imagemCarros.length; i++) {
-        colisao = collideRectCircle(xCarros[i], yCarros[i], comprimentoCarro, alturaCarro, xAtor, yAtor, 25);
+        colisao = collideRectCircle(xCarros[i], yCarros[i] - 14, comprimentoCarro, alturaCarro, xAtor, yAtor, 25);
         if (colisao) {
             voltaAtorPosicaoInicial();
             if (pontosMaiorQueZero()) {
@@ -48,4 +50,8 @@ function marcaPonto() {
 
 function pontosMaiorQueZero() {
     return meusPontos > 0;
+}
+
+function podeSeMover() {
+    return yAtor < 368;
 }
