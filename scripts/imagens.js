31e94e1b3c2151dc3f85882722f0c2358821da04
Victor Rafaeli <victor.srafaeli@gmail.com>
2022-03-08 06:25:27 -0300

    Adiciona sons.

--- scripts/imagens.js
+++ scripts/imagens.js
@@ -4,6 +4,10 @@ let imagemCarro;
 let imagemCarro2;
 let imagemCarro3;
 
+let somDaTrilha;
+let somDaColisao;
+let somDoPonto;
+
 function preload() {
     imagemDaEstrada = loadImage("./resources/img/estrada.png");
     imagemDoAtor = loadImage("./resources/img/ator-1.png");
@@ -11,4 +15,8 @@ function preload() {
     imagemCarro2 = loadImage("./resources/img/carro-2.png");
     imagemCarro3 = loadImage("./resources/img/carro-3.png");
     imagemCarros = [imagemCarro, imagemCarro2, imagemCarro3, imagemCarro, imagemCarro2, imagemCarro3];
+
+    somDaTrilha = loadSound("./resources/sound/trilha.mp3");
+    somDaColisao = loadSound("./resources/sound/colidiu.mp3");
+    somDoPonto = loadSound("./resources/sound/pontos.wav");
 }
